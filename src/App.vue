bc710bc62272bf8a26e54e91007da478418fcbea
Louie <twlouie@wang-line.com>
2023-09-25 17:30:49 +0800

    set environment variable

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,15 @@ import Start from './components/Start.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <!-- <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
+    </a> -->
+    <Start msg="俄羅斯方塊" />
+    <!-- <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    </a> -->
   </div>
   <!-- <HelloWorld msg="Vite + Vue" /> -->
-  <Start msg="幹你老師" />
 </template>
 
 <style scoped>
