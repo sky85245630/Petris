8b94eb1ca8735802f06b61e0231e6c231dd8711b
Louie <twlouie@wang-line.com>
2023-09-26 19:01:06 +0800

    繪製方塊

--- src/tetris.js
+++ src/tetris.js
@@ -51,4 +51,49 @@ function drawGrid() {
     app.stage.addChild(graphics);
 }
 
-drawGrid()
+// 生成pieces
+function drawPiece() {
+    const graphics = new PIXI.Graphics();
+    graphics.beginFill(piece.color);
+    // 遍歷方塊的每個單元格，並在畫布上繪製相應的矩形
+    for (let y = 0; y < piece.shape.length; y++) {
+        for (let x = 0; x < piece.shape[y].length; x++) {
+            if (piece.shape[y][x]) {
+                graphics.drawRect((piece.x + x) * BLOCK_SIZE, (piece.y + y) * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+            }
+        }
+    }
+    app.stage.addChild(graphics);
+}
+
+// 生成方塊
+function spawnPiece() {
+    const randomShape = shapes[Math.floor(Math.random() * shapes.length)];
+    piece.shape = randomShape;
+    piece.x = Math.floor((WIDTH - randomShape[0].length) / 2);
+    piece.y = 0;
+    piece.color = 0xFF0000; // red(hardcode) 
+}
+
+// Initialize the game
+function init() {
+    drawGrid();
+    spawnPiece();
+    drawPiece();
+}
+
+function gameLoop() {
+    app.stage.removeChildren();
+    // piece下降
+    piece.y++;
+
+    // 繪製與更新元件
+    drawGrid();
+    drawPiece();
+
+    // 請求下一幀動畫
+    requestAnimationFrame(gameLoop);
+}
+
+init();
+gameLoop()
